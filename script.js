abefc0cb013918f82275f651d483ee450add0703
racheal <feranmilott@gmail.com>
2024-01-11 13:17:51 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// let icon=document.querySelector(".icon")
+// let info=document.querySelector(".link")
+// console.log(info)
+// icon.addEventListener("click", (e)=>{
+//     e.preventDefault()
+//     info.classList.toggle("link")
+//     // info.style.display="block"
+// })
+let icon=document.querySelector(".icon")
+let info=document.querySelector(".links")
+icon.addEventListener("click",(e)=>{
+    e.preventDefault()
+    info.classList.toggle("lin")
+})
